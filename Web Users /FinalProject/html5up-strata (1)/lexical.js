9e2ef90d4ee6ab06e4f1ba448b2dbac027a4b4b3
Daniel Ocampo <ochampo77@gmail.com>
2021-03-20 23:04:17 -0700

    test

--- Web Users /FinalProject/html5up-strata (1)/lexical.js
+++
@@ -1,7 +0,0 @@
-var scope = "global scope"; // A global variable
-function checkscope() {
- var scope = "local scope"; // A local variable
- function f() { return scope; } // Return the value in scope here
- return f();
-}
-checkscope() // => "local scope"
